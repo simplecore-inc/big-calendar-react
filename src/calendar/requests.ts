cb2a8da4290e14e217e498b9ff54c7c84b95f2fc
lramos33 <oliveira.leonardo3004@gmail.com>
2025-02-15 00:51:09 -0300

    chore: add comments

--- src/calendar/requests.ts
+++ src/calendar/requests.ts
@@ -1,11 +1,13 @@
 import { CALENDAR_ITENS_MOCK, USERS_MOCK } from "@/calendar/mocks";
 
 export const getEvents = async () => {
+  // Increase the delay to better see the loading state
   await new Promise(resolve => setTimeout(resolve, 800));
   return CALENDAR_ITENS_MOCK;
 };
 
 export const getUsers = async () => {
+  // Increase the delay to better see the loading state
   await new Promise(resolve => setTimeout(resolve, 800));
   return USERS_MOCK;
 };

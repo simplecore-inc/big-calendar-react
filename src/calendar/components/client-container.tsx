8034f274646ab1e5cb5e43a58fdcc57afd5a2b92
lramos33 <oliveira.leonardo3004@gmail.com>
2025-04-09 17:18:43 -0300

    chore: improve filter and make it more readable

--- src/calendar/components/client-container.tsx
+++ src/calendar/components/client-container.tsx
@@ -6,6 +6,7 @@ import { isSameDay, parseISO } from "date-fns";
 import { useCalendar } from "@/calendar/contexts/calendar-context";
 
 import { CalendarHeader } from "@/calendar/components/header/calendar-header";
+import { CalendarYearView } from "@/calendar/components/year-view/calendar-year-view";
 import { CalendarMonthView } from "@/calendar/components/month-view/calendar-month-view";
 import { CalendarDayView } from "@/calendar/components/week-and-day-view/calendar-day-view";
 import { CalendarWeekView } from "@/calendar/components/week-and-day-view/calendar-week-view";
@@ -21,17 +22,40 @@ export function ClientContainer({ view }: IProps) {
 
   const filteredEvents = useMemo(() => {
     return events.filter(event => {
-      const itemStartDate = new Date(event.startDate);
-      const itemEndDate = new Date(event.endDate);
+      const eventStartDate = new Date(event.startDate);
+      const eventEndDate = new Date(event.endDate);
 
-      const monthStart = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1);
-      const monthEnd = new Date(selectedDate.getFullYear(), selectedDate.getMonth() + 1, 0);
+      if (view === "year") {
+        const yearStart = new Date(selectedDate.getFullYear(), 0, 1);
+        const yearEnd = new Date(selectedDate.getFullYear(), 11, 31, 23, 59, 59, 999);
+        const isInSelectedYear = eventStartDate <= yearEnd && eventEndDate >= yearStart;
+        const isUserMatch = selectedUserId === "all" || event.user.id === selectedUserId;
+        return isInSelectedYear && isUserMatch;
+      }
 
-      const isInSelectedMonth = itemStartDate <= monthEnd && itemEndDate >= monthStart;
-      const isUserMatch = selectedUserId === "all" || event.user.id === selectedUserId;
-      return isInSelectedMonth && isUserMatch;
+      if (view === "month") {
+        const monthStart = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1);
+        const monthEnd = new Date(selectedDate.getFullYear(), selectedDate.getMonth() + 1, 0);
+        const isInSelectedMonth = eventStartDate <= monthEnd && eventEndDate >= monthStart;
+        const isUserMatch = selectedUserId === "all" || event.user.id === selectedUserId;
+        return isInSelectedMonth && isUserMatch;
+      }
+
+      if (view === "week") {
+        const weekStart = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate());
+        const weekEnd = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate() + 6);
+        const isInSelectedWeek = eventStartDate <= weekEnd && eventEndDate >= weekStart;
+        const isUserMatch = selectedUserId === "all" || event.user.id === selectedUserId;
+        return isInSelectedWeek && isUserMatch;
+      }
+
+      if (view === "day") {
+        const isInSelectedDay = isSameDay(eventStartDate, selectedDate);
+        const isUserMatch = selectedUserId === "all" || event.user.id === selectedUserId;
+        return isInSelectedDay && isUserMatch;
+      }
     });
-  }, [selectedDate, selectedUserId, events]);
+  }, [selectedDate, selectedUserId, events, view]);
 
   const singleDayEvents = filteredEvents.filter(event => {
     const startDate = parseISO(event.startDate);
@@ -51,6 +75,7 @@ export function ClientContainer({ view }: IProps) {
       {view === "month" && <CalendarMonthView singleDayEvents={singleDayEvents} multiDayEvents={multiDayEvents} />}
       {view === "week" && <CalendarWeekView singleDayEvents={singleDayEvents} multiDayEvents={multiDayEvents} />}
       {view === "day" && <CalendarDayView singleDayEvents={singleDayEvents} multiDayEvents={multiDayEvents} />}
+      {view === "year" && <CalendarYearView singleDayEvents={singleDayEvents} multiDayEvents={multiDayEvents} />}
     </div>
   );
 }

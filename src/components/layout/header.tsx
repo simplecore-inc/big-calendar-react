a369982bd219f1aa6f865b90e1dacd6d6fa11814
lramos33 <oliveira.leonardo3004@gmail.com>
2025-04-16 17:20:27 -0300

    chore: add X button

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { ArrowUpRight, Calendar } from "lucide-react";
 
 import { ToggleTheme } from "@/components/layout/change-theme";
+import { Button } from "@/components/ui/button";
 
 export function Header() {
   return (
@@ -38,7 +39,29 @@ export function Header() {
             <ArrowUpRight size={14} className="text-foreground" />
           </Link>
 
-          <ToggleTheme />
+          <div className="flex items-center gap-2">
+            <Button size="icon" asChild variant="ghost">
+              <Link href="https://x.com/leoo_ramos1" target="_blank">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M4 4l11.733 16h4.267l-11.733 -16z" />
+                  <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" />
+                </svg>
+              </Link>
+            </Button>
+
+            <ToggleTheme />
+          </div>
         </div>
       </div>
     </header>

8b548e097fd689abecfd043febba88e4160388c9
lramos33 <oliveira.leonardo3004@gmail.com>
2025-04-19 00:56:00 -0300

    feat: add input to change visible hours

--- src/app/(calendar)/layout.tsx
+++ src/app/(calendar)/layout.tsx
@@ -1,6 +1,9 @@
+import { Settings } from "lucide-react";
+
 import { CalendarProvider } from "@/calendar/contexts/calendar-context";
 
 import { ChangeBadgeVariantInput } from "@/calendar/components/change-badge-variant-input";
+import { ChangeVisibleHoursInput } from "@/calendar/components/change-visible-hours-input";
 
 import { getEvents, getUsers } from "@/calendar/requests";
 
@@ -11,7 +14,16 @@ export default async function Layout({ children }: { children: React.ReactNode }
     <CalendarProvider users={users} events={events}>
       <div className="mx-auto flex max-w-screen-2xl flex-col gap-4 px-8 py-4">
         {children}
-        <ChangeBadgeVariantInput />
+
+        <div className="flex items-center gap-2">
+          <Settings className="size-4" />
+          <p className="text-lg font-semibold">Settings</p>
+        </div>
+
+        <div className="flex flex-col gap-6">
+          <ChangeBadgeVariantInput />
+          <ChangeVisibleHoursInput />
+        </div>
       </div>
     </CalendarProvider>
   );

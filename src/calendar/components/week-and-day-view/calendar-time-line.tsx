e1ce5046e1c013e30a1ce572780cff3fa1ae5050
lramos33 <oliveira.leonardo3004@gmail.com>
2025-04-19 00:55:37 -0300

    chore: hide time line if is outside visible hours

--- src/calendar/components/week-and-day-view/calendar-time-line.tsx
+++ src/calendar/components/week-and-day-view/calendar-time-line.tsx
@@ -1,7 +1,10 @@
 import { format } from "date-fns";
 import { useEffect, useState } from "react";
 
+import { useCalendar } from "@/calendar/contexts/calendar-context";
+
 export function CalendarTimeline() {
+  const { visibleHours } = useCalendar();
   const [currentTime, setCurrentTime] = useState(new Date());
 
   useEffect(() => {
@@ -9,19 +12,24 @@ export function CalendarTimeline() {
     return () => clearInterval(timer);
   }, []);
 
-  const getCurrentTimePosition = () => {
-    const minutes = currentTime.getHours() * 60 + currentTime.getMinutes();
-    return (minutes / 1440) * 100;
-  };
+  const currentHour = currentTime.getHours();
+  const currentMinute = currentTime.getMinutes();
+  const currentTimeInMinutes = currentHour * 60 + currentMinute;
 
-  const formatCurrentTime = () => {
-    return format(currentTime, "h:mm a");
-  };
+  const startTimeInMinutes = visibleHours.from * 60;
+  const endTimeInMinutes = visibleHours.to * 60;
+  const isInVisibleRange = currentTimeInMinutes >= startTimeInMinutes && currentTimeInMinutes <= endTimeInMinutes;
+
+  if (!isInVisibleRange) return null;
+
+  const position = ((currentTimeInMinutes - startTimeInMinutes) / (endTimeInMinutes - startTimeInMinutes)) * 100;
 
   return (
-    <div className="pointer-events-none absolute inset-x-0 z-50 border-t border-primary" style={{ top: `${getCurrentTimePosition()}%` }}>
+    <div className="pointer-events-none absolute inset-x-0 z-50 border-t border-primary" style={{ top: `${position}%` }}>
       <div className="absolute left-0 top-0 size-3 -translate-x-1/2 -translate-y-1/2 rounded-full bg-primary"></div>
-      <div className="absolute -left-18 flex w-16 -translate-y-1/2 justify-end bg-background pr-1 text-xs font-medium text-primary">{formatCurrentTime()}</div>
+      <div className="absolute -left-18 flex w-16 -translate-y-1/2 justify-end bg-background pr-1 text-xs font-medium text-primary">
+        {format(currentTime, "h:mm a")}
+      </div>
     </div>
   );
 }
